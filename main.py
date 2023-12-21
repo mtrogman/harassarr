9e231a7f10cac6750e106fb789b88e00ffd80586
mtrogman <miketrogman@gmail.com>
2023-12-21 15:11:07 -0500

    Cleaned up Functionality Tested and built logic for any config issues Tested and logged validation of connection

--- main.py
+++ main.py
@@ -8,42 +8,28 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)s -
 configFile = "./config/config.yml"
 
 def main():
-    check = configFunctions.checkConfig(configFile)
-    # fix config
-    if check is False:
-        # reprompt for host, table, user, password.  Set the default of these values as what existed in the config (if nothing existed in config utilize the normal default)
-        config = configFunctions.getConfig(configFile)
-        if 'database' not in config:
-            logging.info("Database configuration not found in config.yml. Using default configuration.")
-            # Create a default structure for the config file
-            default_config = {
-                'database': {
-                    'host': '',
-                    'table': 'media_mgmt',
-                    'user': 'lsql_harassarr',
-                    'password': '',
-                }
-            }
-            # Update the existing config with default values
-            config.update(default_config)
-            with open(configFile, 'w') as file:
-                yaml.dump(config, file)
-        
-        # Set default values based on existing config or use normal defaults
-        host = config['database'].get('host', 'default_host')
-        table = config['database'].get('table', 'default_table')
-        user = config['database'].get('user', 'default_user')
-        password = config['database'].get('password', 'default_password')
-        # validate inputs are successful
-        host, table, user, password = dbFunctions.updateDatabaseConfig(host, table, user, password)
-        # update config.yml's database (host, table, user, password) with the input from above
-        configFunctions.updateDBInfo(host, table, user, password)
-        # Validate the connection with the updated values
-        dbFunctions.validate_connection(user, password, host)
-    else:
-        config = configFunctions.getConfig(configFile)
-        dbFunctions.validate_connection(config['database']['user'], config['database']['password'], config['database']['host'])
-        # update config.yml's database (host, table, user, password) if values changed
+    configCheck = configFunctions.checkConfig(configFile)
+    if configCheck:
+        logging.info(f"Configuration file (config.yml) looks good")
+    config = configFunctions.getConfig(configFile)
+    host = config['database']['host']
+    port = config['database']['port']
+    database = config['database']['database']
+    user = config['database']['user']
+    password = config['database']['password']
+    table = "users"
+    serverValidation = configFunctions.validateServer(host, port)
+    if serverValidation:
+        logging.info(f"Server {host} is listening on {port}")
+    databaseValidation = configFunctions.validateDBConnection(user, password, host)
+    if databaseValidation:
+        logging.info(f"Authenticated with {user} to {host} sucessfully")
+    validateDBDatabase = configFunctions.validateDBDatabase(user, password, host, database)
+    if validateDBDatabase:
+        logging.info(f"Database {database} exists on {host}")
+    tableValidation = configFunctions.validateDBTable(user, password, host, database, table)
+    if tableValidation:
+        logging.info(f"Table {table} exists on {database}")
 
 
 if __name__ == "__main__":
--- modules/dbFunctions.py
+++ modules/dbFunctions.py
@@ -1,144 +1,61 @@
 #dbFunctions.py
-import re
 import sys
 import mysql.connector
 import logging
-import socket
-import yaml
-import os
+
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-port = 3306  # MariaDB default port
 
 
-def validate_input(input_string, pattern):
-    while not re.match(pattern, input_string):
-        print("Invalid input. Please try again.")
-        input_string = input()
-    return input_string
-
-
-def validate_server(server, port):
+def createDBUser(root_user, root_password, new_user, new_password, database, server):
     try:
-        # Try connecting to the server on the specified port
-        with socket.create_connection((server, port), timeout=1):
-            pass
-        return True
-    except (socket.timeout, socket.error):
-        return False
-
-
-def updateDatabaseConfig(host, table, user, password):
-    host = input(f"Enter host (default is {host}): ") or host
-    table = input(f"Enter table (default is {table}): ") or table
-    user = input(f"Enter user (default is {user}): ") or user
-    password = input("Enter new password (or to keep same password leave blank and hit enter): ") or password
-    cnx = mysql.connector.connect(user=user, password=password, host=server)
-    if cnx:
-        cnx.close()
-    else:
-        if not validate_server(server, port):
-            while True:
-                logging.error(f"Could not connect to the database on port 3306. Please enter a valid IP address or hostname.")
-                server = get_validated_input("Enter IP address or hostname of database: ", r'^[a-zA-Z0-9.-]+$')
-
-                if validate_server(server, port):
-                    break
-        else:
-            while True:
-                logging.error(f"Could not connect to the database with the credentials provided.  Please enter valid creds.")
-                user = input("Enter new username (default is lsql_harassarr): ") or 'lsql_harassarr'
-                password = input("Enter new user password: ")
-                cnx = mysql.connector.connect(user=user, password=password, host=server)
-                if cnx:
-                    cnx.close()
-                    break
-                else:
-                    logging.error(f"Could not connect to the database with the credentials provided.  Please enter valid creds.")
-    return host, table, user, password
-
-def validate_connection(user, password, server):
-    cnx = mysql.connector.connect(user=user, password=password, host=server)
-    if cnx:
-        cnx.close()
-    else:
-        logging.error(f"Could not connect to the database with information in config.yml")
-        if not validate_server(server, port):
-            logging.error(f"Could not connect to the database on port 3306. Please enter a valid IP address or hostname.")
-            while True:
-                server = get_validated_input("Enter IP address or hostname of database: ", r'^[a-zA-Z0-9.-]+$')
-
-                if validate_server(server, port):
-                    break
-                else:
-                    logging.error(f"Could not connect to the database on port 3306. Please enter a valid IP address or hostname.")
-        else:
-            logging.error(f"Could not connect to the database with the credentials provided.  Please enter valid creds.")
-            while True:
-                user = input("Enter new username (default is lsql_harassarr): ") or 'lsql_harassarr'
-                password = input("Enter new user password: ")
-                # Connect to MySQL using the root user to create the new user
-                cnx = mysql.connector.connect(user=user, password=password, host=server)
-                if cnx:
-                    cnx.close()
-                    break
-                else:
-                    print("Could not connect to the database with the credentials provided.  Please enter valid creds.")
-
-
-
-def get_validated_input(prompt, pattern):
-    user_input = input(prompt)
-    user_input = validate_input(user_input, pattern)
-    return user_input
-
-
-def get_database_user_input():
-    while True:
-        server = get_validated_input("Enter IP address or hostname of database: ", r'^[a-zA-Z0-9.-]+$')
-
-        if validate_server(server, port):
-            break
-        else:
-            print("Could not connect to the database on port 3306. Please enter a valid IP address or hostname.")
-
-    while True:
-        root_user = input("Enter root database user (default is root): ") or 'root'
-        root_password = input("Enter root database password: ")
-        # Connect to MySQL using the root user to create the new user
+        # Connect to MySQL using the root user to check if the user already exists
         cnx = mysql.connector.connect(user=root_user, password=root_password, host=server)
-        if cnx:
-            cnx.close()
-            break
+        cursor = cnx.cursor()
+
+        # Check if the user already exists
+        cursor.execute(f"SELECT 1 FROM mysql.user WHERE user = '{new_user}' LIMIT 1;")
+        user_exists = cursor.fetchone()
+
+        if user_exists:
+            # User already exists, reset the password and update permissions
+            cursor.execute(f"SET PASSWORD FOR '{new_user}'@'%' = PASSWORD('{new_password}');")
+            cursor.execute(f"GRANT ALL PRIVILEGES ON {database}.* TO '{new_user}'@'%';")
+            cursor.execute("FLUSH PRIVILEGES;")
+            logging.info(f"User '{new_user}' exists. Password reset and permissions updated.")
+
         else:
-            print("Could not connect to the database with the credentials provided.  Please enter valid creds.")
+            # User doesn't exist, create the new user with all privileges on the specified database
+            cursor.execute(f"CREATE USER '{new_user}'@'%' IDENTIFIED BY '{new_password}';")
+            cursor.execute(f"GRANT ALL PRIVILEGES ON {database}.* TO '{new_user}'@'%';")
+            cursor.execute("FLUSH PRIVILEGES;")
+            logging.info(f"User '{new_user}' created with all privileges on database '{database}'.")
 
-    new_user = input("Enter new username (default is lsql_harassarr): ") or 'lsql_harassarr'
-    new_password = input("Enter new user password: ")
-    database = input("Enter database name (default is media_mgmt): ") or 'media_mgmt'
+    except mysql.connector.Error as err:
+        logging.error("Database user creation or update failed.")
+        logging.error(f"Error: {err}")
+        return False  # Return failure
 
-    return root_user, root_password, new_user, new_password, database, server
+    finally:
+        # Close the cursor and connection
+        if 'cursor' in locals():
+            cursor.close()
+        if 'cnx' in locals():
+            cnx.close()
+    return True  # Return success
 
 
-def create_database_and_table(root_user, root_password, new_user, new_password, database, server):
+
+def createDBStructure(root_user, root_password, database, server):
     try:
         # Connect to MySQL using the root user to create the new user
         cnx = mysql.connector.connect(user=root_user, password=root_password, host=server)
         cursor = cnx.cursor()
 
-        # Create the new user with all privileges on the specified database
-        cursor.execute(f"CREATE USER '{new_user}'@'%' IDENTIFIED BY '{new_password}';")
-        cursor.execute(f"GRANT ALL PRIVILEGES ON {database}.* TO '{new_user}'@'%';")
-        cursor.execute("FLUSH PRIVILEGES;")
-        logging.info(f"User '{new_user}' created with all privileges on database '{database}'.")
-
-        # Append database information to the config.yml file
-        append_database_to_config(new_user, new_password, database, server, CONFIG_FILE)
-
         # Create the database if it doesn't exist
         cursor.execute(f"CREATE DATABASE IF NOT EXISTS {database};")
-        logging.info(f"Database '{database}' created.")
+        logging.info(f"Database '{database}' in place.")
 
         # Switch to the new database
         cursor.execute(f"USE {database};")
@@ -177,8 +94,9 @@ def create_database_and_table(root_user, root_password, new_user, new_password, 
         cnx.commit()
 
     except mysql.connector.Error as err:
+        logging.error("Database and table creation failed.")
         logging.error(f"Error: {err}")
-        return False  # Return failure
+        return False  
 
     finally:
         # Close the cursor and connection
@@ -186,24 +104,4 @@ def create_database_and_table(root_user, root_password, new_user, new_password, 
             cursor.close()
         if 'cnx' in locals():
             cnx.close()
-    return True  # Return success
-
-
-if __name__ == "__main__":
-    # Read command-line arguments
-    root_user, root_password, new_user, new_password, database, server = get_database_user_input()
-
-    # Call the function to create the user, database, and table
-    success = create_database_and_table(
-        root_user=root_user,
-        root_password=root_password,
-        new_user=new_user,
-        new_password=new_password,
-        database=database,
-        server=server
-    )
-
-    if success:
-        logging.info("User, database, and table creation succeeded.")
-    else:
-        logging.error("User, database, and table creation failed.")
+    return True
